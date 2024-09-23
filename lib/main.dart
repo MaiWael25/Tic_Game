afc5164b16befe97b506ca14e4349327fa3554e1
MaiWael <maiw8757@gmail.com>
2024-09-23 13:03:37 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tic_game/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       brightness: Brightness.dark,
+       primarySwatch: Colors.blue,
+       primaryColor: const Color(0xFF00061a),
+       shadowColor: const Color(0xFF001456),
+       splashColor: const Color(0xFF4169e8)
+
+      ),
+      home: const HomePage()
+    );
+  }
+}
+
